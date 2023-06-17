0edbdbf0f5bb761a50bb41300ce366efabfa3542
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-06-17 21:57:25 +0600

    route setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import routes from "./app/routes";
 const app: Application = express();
 
 // Application routes
@@ -11,6 +12,8 @@ app.use(cors());
 // parse data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1", routes);
 
 // app.get("/", (req, res) => {
 //   res.send("app is running");
