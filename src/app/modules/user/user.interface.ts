c93a0b6f10f751fe35dfce5a98804972bd30d0da
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-06-17 21:40:07 +0600

    user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,19 @@
+import { Model, Types } from "mongoose";
+
+export type UserName = {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+};
+
+export type IUser = {
+  id: string;
+  phoneNumber: string;
+  role: "seller" | "buyer";
+  password: string;
+  name: UserName;
+  address: string;
+  budget: number;
+  income: number;
+};
+export type UserModel = Model<IUser, Record<string, unknown>>;
