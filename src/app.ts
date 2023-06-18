13fd97fa734c861c2b895cc4b26c59fbd964f5b1
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-06-18 18:36:00 +0600

    not found

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import routes from "./app/routes";
@@ -21,5 +21,20 @@ app.use("/api/v1", routes);
 
 //global error handler
 app.use(globalErrorHandler);
+
+// handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "Api Not Found",
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
